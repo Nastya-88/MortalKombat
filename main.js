e46f9f37a6b51e1b23a9f032dc0f79bb3d3c5fa7
Анастасия <shishlakovanastya@gmail.com>
2021-06-09 23:25:09 +0300

    homework-5

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const $arenas = document.querySelector('.arenas');
+const $formFight = document.querySelector('.control');
 const $randomButton = document.querySelector('.button');
 
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+};
+const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
     name: 'Liukang',
@@ -8,12 +15,10 @@ const player1 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: ['Молот Гнева', 'Нагината', 'Кастеты', 'Ударный топор'],
-    attack: function () {
-        console.log(this.name + '' + 'Fight...');
-    },
-    changeHP: changeHP,
-    elHP: elHP,
-    renderHP: renderHP,
+    attack,
+    changeHP,
+    elHP,
+    renderHP,
 };
 
 const player2 = {
@@ -22,14 +27,15 @@ const player2 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['Ледяной скипетр', 'Кодачи', 'Клык демона', 'Секира'],
-    attack: function () {
-        console.log(this.name + '' + 'Fight...');
-    },
-    changeHP: changeHP,
-    elHP: elHP,
-    renderHP: renderHP,
+    attack,
+    changeHP,
+    elHP,
+    renderHP,
 };
 
+function attack() {
+    console.log(this.name + '' + 'Fight...');
+}
 
 function getRandom(num) {
     return Math.ceil(Math.random() * num);
@@ -88,18 +94,48 @@ function createReloadButton() {
         window.location.reload();
     })
     $reloadWrap.appendChild($button);
-    return $reloadWrap;
-
+    $arenas.appendChild($reloadWrap);
 };
 
-$randomButton.addEventListener('click', function () {
-    player1.changeHP(getRandom(20));
-    player1.renderHP();
-    player2.changeHP(getRandom(20));
-    player2.renderHP();
+// $randomButton.addEventListener('click', function () {
+//     player1.changeHP(getRandom(20));
+//     player1.renderHP();
+//     player2.changeHP(getRandom(20));
+//     player2.renderHP();
+//     if (player1.hp === 0 || player2.hp === 0) {
+//         $randomButton.disabled = true;
+//         createReloadButton();
+
+//     }
+
+//     if (player1.hp === 0 && player1.hp < player2.hp) {
+//         $arenas.appendChild(playerWins(player2.name));
+//     } else if (player2.hp === 0 && player2.hp < player1.hp) {
+//         $arenas.appendChild(playerWins(player1.name));
+//     } else if (player1.hp === 0 && player2.hp === 0) {
+//         $arenas.appendChild(playerWins());
+//     }
+
+// });
+
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
+
+
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence,
+    }
+};
+
+function showMessage() {
     if (player1.hp === 0 || player2.hp === 0) {
         $randomButton.disabled = true;
-        $arenas.appendChild(createReloadButton());
+        createReloadButton();
     }
 
     if (player1.hp === 0 && player1.hp < player2.hp) {
@@ -109,8 +145,41 @@ $randomButton.addEventListener('click', function () {
     } else if (player1.hp === 0 && player2.hp === 0) {
         $arenas.appendChild(playerWins());
     }
+}
 
+$formFight.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const enemy = enemyAttack();
+    const attack = {};
+
+    for (let item of $formFight) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value;
+        }
+        if (item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+        item.checked = false;
+    }
+
+    if (attack.defence !== enemy.hit) {
+        player1.changeHP(attack.value);
+        player1.renderHP();
+    }
+    if (enemy.defence !== attack.hit) {
+        player2.changeHP(enemy.value);
+        player2.renderHP();
+
+    }
+    if (attack.hit === enemy.defence) {
+        player2.changeHP(0);
+        player2.renderHP();
+
+    }
+    if (enemy.hit === attack.defence) {
+        player1.changeHP(0);
+        player1.renderHP();
+    }
+    showMessage();
 });
-
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
